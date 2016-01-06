a253e62b51494df187b440e9e6c8cb242e9e52a0
Jonathan Heek <heekjonathan@gmail.com>
2016-01-07 00:33:08 +0100

    add date

--- public/app/controllers/dashboard.js
+++ public/app/controllers/dashboard.js
@@ -1,9 +1,21 @@
 
+const days = ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'];
+const months = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
+
 export default class DashboardController {
 
-  constructor($scope) {
+  constructor($scope, $timeout) {
+    function updateDate() {
+      let now = new Date();
+      $scope.dayOfWeek = days[now.getDay()];
+      $scope.dayOfMonth = now.getDate();
+      $scope.month = months[now.getMonth()];
+      $scope.year = now.getFullYear();
+      $timeout(updateDate, 1000 * 60 * 60); // every hour
+    }
+    updateDate();
   }
 
 }
 
-DashboardController.$inject = ['$scope'];
+DashboardController.$inject = ['$scope', '$timeout'];
