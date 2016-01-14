a04dbff7243746d2cc5b5b2f18e3ae6e01af7683
Jonathan Heek <heekjonathan@gmail.com>
2016-01-14 21:04:44 +0100

    Update dashboard with date styling

--- public/app/directives/dashboard.js
+++ public/app/directives/dashboard.js
@@ -6,14 +6,20 @@ const months = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 
 function getTime(date) {
   let mins = Math.round(date.getMinutes() / 5) * 5;
   let direction = mins < 30;
-  mins = direction ? mins : 60 - mins;
   let hours = date.getHours() % 12;
+  if (mins === 60) {
+    hours += 1;
+  }
+  mins = direction ? mins : 60 - mins;
+
 
   let isHalf = mins > 15;
   if (isHalf) {
     mins = 30 - mins;
     direction = !direction;
     mins = Math.abs(mins);
+  } else if (!direction) {
+    hours++;
   }
 
   if (mins === 15) {
