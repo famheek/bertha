dfb382bd5f6bb8a6864a48ebbe03b1e0216830ca
Jonathan Heek <heekjonathan@gmail.com>
2016-03-01 15:09:56 +0100

    language fix, removed responsive voice logs

--- public/lib/speech.js
+++ public/lib/speech.js
@@ -1,13 +1,3 @@
 export function speak(text) {
-  responsiveVoice.speak(text, 'Dutch Female', {
-    onstart: function() {
-      console.log('begin');
-    },
-    onend: function() {
-      console.log('uitgesproken');
-    },
-    onerror: function() {
-      console.log('error');
-    }
-  });
+  responsiveVoice.speak(text, 'Dutch Female');
 }
