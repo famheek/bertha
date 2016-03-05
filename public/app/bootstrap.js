3b8283c6bf390ef26b9a008df1340e21c560ad0d
Ruben Heek <rubenheek@icloud.com>
2016-03-05 17:58:52 +0100

    turned off midnight reload for now, deleted unnecessary angular import

--- public/app/bootstrap.js
+++ public/app/bootstrap.js
@@ -1,21 +1,20 @@
-import angular from 'angular';
 import mainModule from './main';
 
-import {schedule} from '../lib/timer';
+//import {schedule} from '../lib/timer';
 
 angular.element(document).ready(function() {
     angular.bootstrap(document, [mainModule.name]);
 });
 
-function scheduleReload() {
-  let reloadDate = new Date();
-  reloadDate.setMilliseconds(1);
-  reloadDate.setHours(0);
-  reloadDate.setMinutes(0);
+// function scheduleReload() {
+//   let reloadDate = new Date();
+//   reloadDate.setMilliseconds(1);
+//   reloadDate.setHours(0);
+//   reloadDate.setMinutes(0);
 
-  reloadDate.setDate(reloadDate.getDate() + 1);
+//   reloadDate.setDate(reloadDate.getDate() + 1);
 
-  schedule(reloadDate).then(() => location.reload());
-}
+//   schedule(reloadDate).then(() => location.reload());
+// }
 
-scheduleReload();
+// scheduleReload();
